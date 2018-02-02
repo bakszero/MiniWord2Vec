83728682377d02a81c94ad7163fa40818cd03394
Bakhtiyar <syed.b@research.iiit.ac.in>
2018-02-02 17:24:56 +0530

    Updated utils

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,6 @@
 import sys
 #import keras 
+from operator import itemgetter
 
 def remove_new_line(file, out):
 	with open(file, 'r') as f, open(out, 'w+') as g:
@@ -19,10 +20,18 @@ def do_something(file):
 	for word in text:
 		if word in vocab:
 			vocab[word]+=1
+			continue
+			#print (word, vocab[word])
+
 		vocab[word] = 1
 
 	return vocab
 #remove_new_line(sys.argv[1], sys.argv[2])
 vocab = do_something(sys.argv[1])
 
+print (len(vocab))
+
+sorted_vocab = sorted(vocab.items(), key = itemgetter(1))
+for key, value in sorted_vocab:
+	print ("%s %s" % (key, value)	)
 print (len(vocab))
